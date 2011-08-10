cf946782d4d5fb90f56dc35612e8aea6d30b435f
Sasank Mudunuri <sasank@tomato.local>
2011-08-10 14:48:49 -0700

    Created new rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schdlme::Application.config.secret_token = '8486e7fcfc48546ae14765f4e3a2892a9c093c885fcd9b7be2d514a18f455ed798a6bd72faefd8327eb32b6d96ed85ea18d215e2e639cfb7b4ed81fe7761e82b'
